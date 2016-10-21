6bf8eb7cf804e78433d24eb52827ed57752c5a8c
ABartles <andrew.j.bartles@gmail.com>
2016-10-20 19:52:18 -0700

    initial Assignment3_Factorial/

---
+++ Assignment3_Factorial/Assignment3_Factorial/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment3_Factorial
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Ask the user for a numerical input.
+            Console.Write("Please enter a non negitive/zero value: ");
+           
+            // Store the users input as a str type 
+            string userstr = Console.ReadLine();
+           
+            // Convert character into an int. 
+            int userint = int.Parse(userstr);
+
+            // Set up a 2nd variable for the equation.
+            int ans = userint;
+            
+            // Calculate the factorial of the users value.
+            while (userint > 1)
+            {
+                ans = ans * (userint - 1);
+                userint--;                
+            }
+
+            // Show the output of the factorial.
+            Console.WriteLine("The factorial of {0} is: {1}", userstr, ans);
+            
+            // Hold the window open.
+            Console.ReadLine();
+        }
+    }
+}
