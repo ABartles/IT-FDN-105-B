32a845875d9038fdaaa857e442857a620e2a144c
ABartles <andrew.j.bartles@gmail.com>
2016-11-06 18:19:28 -0800

    Adding course exercises.

---
+++ Module6_Ex2/Module6_Ex2/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Module6_Ex2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write(" Here is the multiplication for 5x5 table: ");   //creating a 5x5 matrix (or a table)
+            Console.WriteLine();
+            Console.WriteLine();
+
+            int x; // declare an integer x to keep track of rows
+            int y; // declare an integer y to keep track of columns
+
+
+            // first for loop creates the rows
+            for ( x=1; x<=5; x++) //code here to create the x rows, marches vertically one row at a time
+            {
+
+                // second for loop creates the columns
+                for ( y=1; y<=5; y++) // code here for creating y columns, marches horizontally as spaced below
+                {
+                    int z = x * y; // multiply x by y to create a value that gets placed into the table
+
+
+                    // Note: {0,6} indicates 6 spaces for parameter zero
+                    Console.Write("{0,6}", z);
+
+
+                } // end of column loop
+
+                Console.WriteLine(); // a line or a space
+
+            } // end of row loop
+
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ module6_Ex1/Module6_EX1/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Module6_EX1
+{
+    class Program
+    {
+        static void Main()
+        {
+            int[] numbers = new int[] { 10, 15, 20, 25, 30, 35 };
+
+            Console.Write("Enter a number: ");
+            string str = Console.ReadLine();
+
+            int num = int.Parse(str);  // convert input string into a number
+            bool found = false;          // set a boolean flag
+
+            foreach (int x in numbers) // use foreach to look for the number
+            {
+                // if number is found set found flag to true
+                if (num == x)
+                {
+                    found = true;
+                }
+            }
+
+            if (found == true) // if true, i.e. found the number, say Found the Number
+            {
+                Console.WriteLine("Found the number.");
+            }
+            else // if not true, did not find it, say Did not Find the Number
+            {
+                Console.WriteLine("Did not find the number.");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
